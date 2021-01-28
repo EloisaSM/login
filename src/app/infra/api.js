15a4ee3a56a4faf535dbba1c198364e893a5f3ee
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-01-28 14:53:29 -0300

    requisição para rota users

--- src/app/infra/api.js
+++ src/app/infra/api.js
@@ -1,15 +1,8 @@
-function requestApi(form,cb) {
+function requestLoginApi(form,cb) {
     let xhr = new XMLHttpRequest()
 
     xhr.open('POST', 'http://localhost:3000/auth', true)
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
-    
-    // xhr.onreadystatechange = function() {
-    //     if(this.readyState && this.status) {
-    //         const response = this.responseText;
-            
-    //     }
-    // }
 
     xhr.addEventListener('load', function() {
         const bodyResponse = xhr.responseText;
@@ -18,6 +11,27 @@ function requestApi(form,cb) {
         cb(bodyResponse, statusCode);
     });
     
+    const formData = new FormData(form)
+    const convertFormData = new URLSearchParams(formData).toString()
+    console.log(convertFormData)
+    xhr.send(convertFormData) 
+}
+
+function requestNewUser(form, cb) {
+    let xhr = new XMLHttpRequest()
+
+    xhr.open('POST', 'http://localhost:3000/users', true)
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+
+    xhr.addEventListener('load', function() {
+        const bodyResponse = xhr.responseText;
+        const statusCode = this.status;
+
+        console.log(bodyResponse, statusCode)
+
+        cb(bodyResponse, statusCode);
+    });
+
     const formData = new FormData(form)
     const convertFormData = new URLSearchParams(formData).toString()
     xhr.send(convertFormData) 
