91d7a36546f0e855a250822724d24e03bef17d44
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-02-01 16:55:44 -0300

    limpar message container botão click hear to sign up

--- src/app/infra/pop-up.js
+++ src/app/infra/pop-up.js
@@ -4,9 +4,13 @@ const loginContainer = document.querySelector('.login-container');
 function showRegisterContainer() {
     registerContainer.classList.add('show');
     loginContainer.classList.add('hide');
+
+    const containerMessage = document.querySelector('.messages-container')
+    containerMessage.innerHTML = ''
 }
 
 function hideRegisterContainer(){
     registerContainer.classList.remove('show');
     loginContainer.classList.add('show');
 }
+
