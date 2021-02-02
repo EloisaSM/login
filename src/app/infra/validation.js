025147f146d0da07bd32306ebd912c9bd5984343
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-02-02 14:16:46 -0300

    mensagem autenticação e limpar mensagem de erro autenticação

--- src/app/infra/validation.js
+++ src/app/infra/validation.js
@@ -1,6 +1,7 @@
 function sendForm(event, form, path) {
     event.preventDefault()
     requestApi('/auth', form, auth) 
+    clearErrorMessage(form)
     reset(form)
 }
 
@@ -13,13 +14,20 @@ const setErrorMessage = (errorMessage) => {
     containerMessage.innerText = errorMessage
 }
 
+const clearErrorMessage = (form) => {
+    form.addEventListener('input', function (){
+        const containerMessage = document.querySelector('.messages-container')
+        containerMessage.innerText = ''
+    })
+}
+
 function auth(bodyRequestResponse, statusCode) {
     if(statusCode === 401) {
         setErrorMessage(bodyRequestResponse)
     }
 
     if(statusCode === 200) {
-        alert('Sucesso')
+        alert('Bem vindo!')
     }
 }
 
